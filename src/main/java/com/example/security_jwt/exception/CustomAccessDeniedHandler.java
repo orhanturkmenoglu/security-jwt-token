244f0516c8ea40e7cd17ce2854c4bd1ebea1212b
ORHAN TÜRKMENOĞLU <orhantrkmn749@gmail.com>
2024-10-03 15:01:31 +0300

    feat: CustomAccessDeniedHandler.java correction made.

--- src/main/java/com/example/security_jwt/exception/CustomAccessDeniedHandler.java
+++ src/main/java/com/example/security_jwt/exception/CustomAccessDeniedHandler.java
@@ -10,7 +10,6 @@ import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.time.LocalDateTime;
 import java.util.Map;
 
 
